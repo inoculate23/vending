d9688882d2007c30481192d35c3322f475bff878
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2024-01-16 15:02:06 -0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ document.querySelector('#app').innerHTML = `
 
 <div class="hero min-h-screen bg-base-200">
   <div class="hero-content flex-col lg:flex-row">
-    <img src="./src/images/pexels-photo-5196187.jpeg" class="max-w-sm rounded-lg shadow-2xl" />
+    <img src="./images/pexels-photo-5196187.jpeg" class="max-w-sm rounded-lg shadow-2xl" />
     <div>
       <h1 class="text-5xl font-bold">Box Office News!</h1>
       <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
