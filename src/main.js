43d48063751ed326e6b673bd02c513f79967949e
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2024-01-24 19:16:05 -0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,23 +1,38 @@
 import './app.css';
 
 document.querySelector('#app').innerHTML = `
+
 <div class="navbar bg-dark text-neutral-content">
-<button class="btn btn-neutral text-xl">Sin City Sweets</button>
-
-<a href="#machines"><button  class="btn btn-secondary text-l">Our Machines</button></a>
-
-
-<button class="btn btn-primary text-l">Contact</button>
+<div class="flex-1">
+<a href="./index.html"><button class="btn btn-secondary text-xl">Sin City Sweets</button></a>
 </div>
+<div class="flex-none">
+<a href="#machines"><button  class="btn btn-primary text-l">Need a Vending Machine?</button></a>
 
+
+
+<a href="#about"><button class="btn btn-primary text-l">About Us</button></a>
+
+<a href="#contact"><button class="btn btn-primary text-l">Contact</button></a>
+<a href="tel:7025507688"><button class="btn btn-primary text-l">Phone</button></a>
+</div>
+</div>
 <div class="hero min-h-screen bg-base-200">
   <div class="hero-content flex-col lg:flex-row">
+  
   <iframe id="iframe" title="#3December 2023 - The Vendomatic 360" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/95e89daf76724f2f9eb1725ce52cbe39/embed?autospin=1&autostart=1&ui_infos=0&ui_watermark_link=0&ui_watermark=0&ui_hint=0&ui_theme=dark" width="100%" height="100vh"> </iframe> 
 
     <div>
-      <h1 class="text-5xl font-bold">Box Office News!</h1>
-      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button class="btn btn-primary">Get Started</button>
+      <h1 class="text-5xl text-primary font-bold">VendGo Machines 
+      </h1>
+      <br>
+      <h2 class="text-2xl text-accent font-bold"> in Our Business</h2>
+      <p class="py-6">At Sin City Sweets Vending, we’re proud to offer our vending machines that dispense snacks and drinks to your residents. We make it easy for them to grab either a necessary drink or snack without having to leave the premises.</p>
+      <h2 class="text-2xl text-accent font-bold">Convenience</h2>
+      <p class="py-6">Sin City Sweets Vending currently operates machines at several establishments in our community. We plan to make it easy for your residents to quickly grab items. It's far more convenient for them to step downstairs (perhaps in their slippers) than to walk to a store.</p>
+      <h2 class="text-2xl text-accent font-bold">The Bottom-Line</h2>
+      <p class="py-6"> Our data shows that in-house vending machines capture more revenue simply because of their visibility and convenience.</p>
+     <a href="#machines"> <button class="btn btn-primary">Get Started</button></a>
     </div>
   </div>
 </div>
@@ -70,6 +85,122 @@ document.querySelector('#app').innerHTML = `
 </div>
 </div>
 
+</div>
+
+</section>
+
+<section id="about">
+<div class="hero max-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">The SinCity Dream Team</h1>
+      </div>
+    
+<div class="card join-horizontal w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+  
+  <figure><img src="/images/david.png" alt="David" /></figure>
+
+  <h1 class="text-2xl text-primary font-bold">David J. Frias</h1>
+<p class="text-sm text-accent">CO-CHIEF EXECUTIVE OFFICER</p>
+
+    </div>
+  </div>
+
+<div class="card join-horizontal w-96 bg-base-100 shadow-xl">
+<div class="card-body">
+  
+  <figure><img src="/images/rich.png" alt="Richard" /></figure>
+
+  <h1 class="text-2xl text-primary font-bold">Richard Lang
+
+  </h1>
+<p class="text-sm text-accent">CO-CHIEF EXECUTIVE OFFICER</p>
+
+    </div>
+  </div>
+
+  <div class="card join-horizontal w-96 bg-base-100 shadow-xl">
+<div class="card-body">
+  
+  <figure><img src="/images/sam.png" alt="Samantha" /></figure>
+
+  <h1 class="text-2xl text-primary font-bold">Samantha Kormoski</h1>
+<h3 class="text-sm text-accent">DIRECTOR OF MARKETING
+</h3>
+
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<center>
+<div class="stats shadow">
+  
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    </div>
+    <div class="stat-title">Happy Clients</div>
+    <div class="stat-value">100</div>
+  
+    </div>
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+    </div>
+    <div class="stat-title">Partners in USA</div>
+    <div class="stat-value">50+</div>
+    <div class="stat-desc">↗︎ 25 (50%)</div>
+  </div>
+  
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+    </div>
+    <div class="stat-title">Candy Bars Sold</div>
+    <div class="stat-value">20,000</div>
+    <div class="stat-desc">↗︎ 5000 (25%)</div>
+  </div>
+  
+  <div class="stat">
+  <div class="stat-figure text-secondary">
+  
+  <img src="/images/water.svg" width="40px">
+  </div>
+  <div class="stat-title">Thirsts Quenched</div>
+  <div class="stat-value">32,080</div>
+  <div class="stat-desc">↗︎ 8000 (25%)</div>
+</div>
+</div>
+</center>
+</section>
+<section id="contact">
+
+<div class="contact-box">
+
+ 
+  
+<img src="/images/woman.webp" width="45%"/><div class="contact-form-wrapper">
+  <form>
+    <div class="form-item text-accent text-3xl">
+      <input type="text" name="sender" required>
+      <label class="text-accent text-2xl">Name:</label>
+    </div>
+    <div class="form-item">
+      <input type="text" name="email" required>
+      <label class="text-accent text-2xl">Email:</label>
+    </div>
+    <div class="form-item">
+      <textarea class="" name="message" required></textarea>
+      <label class="text-accent text-2xl">Message:</label>
+    </div>
+    <button class="submit-btn bg-primary">Send</button>  
+  </form>
+</div>
 </div>
 
 </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/images/**/*.(png,jpg,jpeg}"],
-  theme: {
-    extend: {},
+  plugins: [require('daisyui')],
+
+  // config docs: https://daisyui.com/docs/config/
+  daisyui: {
+    themes: ["light", "dark", "dracula", "night", "dim", "cupcake","pastel", "wireframe", "nord", "retro" , "synthwave", "cyberpunk", "lofi", "business", "aqua", "forest"],
   },
-  plugins: [require("daisyui")],
+  darkTheme: "dark",
+  base: true, // applies background color and foreground color for root element by default
+  styled: true, // include daisyUI colors and design decisions for all components
+  utils: true, // adds responsive and modifier utility classes
+  prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+  logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+  themeRoot: ":root"
 }
+
