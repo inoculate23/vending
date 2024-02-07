4b823bfe33d0471af8dbc7700ca130593b9c32c7
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2024-02-06 16:56:33 -0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ document.querySelector('#app').innerHTML = `
 </div>
 <div class="flex-none">
 <a href="#machines"><button  class="btn btn-primary text-l">Need a Vending Machine?</button></a>
+<a href="#benefits"><button class="btn btn-primary text-l">Benefits</button></a>
+<a href="#stats"><button class="btn btn-primary text-l">Statistics</button></a>
 
 
 
@@ -36,7 +38,7 @@ document.querySelector('#app').innerHTML = `
     </div>
   </div>
 </div>
-
+<div class="divider"></div> 
 <section id="machines">
 <div class="join join-vertical lg:join-horizontal">
   
@@ -89,11 +91,106 @@ document.querySelector('#app').innerHTML = `
 
 </section>
 
+<div class="divider"></div> 
+
+<section id="benefits">
+<center>
+<h1 class="text-5xl font-bold text-primary">Benefits & Placement</h1>
+<div class="join join-vertical lg:join-horizontal">
+  
+  <div class="card join-item w-86 bg-base-100 shadow-xl">
+  <div class="card-body join-item">
+    <h2 class="card-title join-item">Multiple Forms of Payment</h2>
+    <p>For greater flexibility, our machines accept credit and debit cards as well as cash. Our machines also use a system called “PayRange” which is something used at most laundry facilities today.</p>
+  </div>
+</div>
+
+<div class="card join-item w-86 bg-base-100 shadow-xl">
+<div class="card-body join-item">
+  <h2 class="card-title join-item">Convenience</h2>
+  <p>Your residents can take advantage of having snacks, juices, detergent for laundry instead of walking to the nearest convenience store We rotate our snacks to ensure the quality and freshness of our snacks and products.</p>
+</div>
+</div>
+
+<div class="card join-item w-86 bg-base-100 shadow-xl">
+<div class="card-body join-item">
+  <h2 class="card-title join-item">Products</h2>
+  <p>We will stock machines with a wide variety of items. The items will range from: Snacks, Chips, Candy, Soap (if needed) or small household items requested.</p>
+</div>
+</div>
+
+<div class="card join-item w-86 bg-base-100 shadow-xl">
+
+<div class="card-body join-item">
+  <h2 class="card-title join-item">Quality Control</h2>
+  <p>All our products are packaged with dates of expirations listed. By placing close attention to these absolute dates, we ensure the quality remains across or product line within our vending machines.
+  </p>
+</div>
+</div>
+
+</div>
+<center>
+<div>
+<h1 class="text-5xl font-bold text-secondary">All the benefits at Zero cost to you!</h1>
+</div>
+<div class="divider"></div> 
+</center>
+</section>
+
+<section id="stats">
+<center>
+<h1 class="text-5xl font-bold">Statistics</h1>
+<div class="stats shadow">
+
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    </div>
+    <div class="stat-title">Happy Clients</div>
+    <div class="stat-value">100</div>
+  
+    </div>
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+    </div>
+    <div class="stat-title">Partners in USA</div>
+    <div class="stat-value">50+</div>
+    <div class="stat-desc">↗︎ 25 (50%)</div>
+  </div>
+  
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+    </div>
+    <div class="stat-title">Candy Bars Sold</div>
+    <div class="stat-value">20,000</div>
+    <div class="stat-desc">↗︎ 5000 (25%)</div>
+  </div>
+  
+  <div class="stat">
+  <div class="stat-figure text-secondary">
+  
+  <img src="/images/water.svg" width="40px">
+  </div>
+  <div class="stat-title">Thirsts Quenched</div>
+  <div class="stat-value">32,080</div>
+  <div class="stat-desc">↗︎ 8000 (25%)</div>
+</div>
+</div>
+</center>
+</section>
+
+<div class="divider"></div> 
+
 <section id="about">
+<center>
+<h1 class="text-5xl font-bold">The SinCity Dream Team</h1>
+</center>
 <div class="hero max-h-screen bg-base-200">
   <div class="hero-content text-center">
     <div class="max-w-md">
-      <h1 class="text-5xl font-bold">The SinCity Dream Team</h1>
+
       </div>
     
 <div class="card join-horizontal w-96 bg-base-100 shadow-xl">
@@ -137,53 +234,13 @@ document.querySelector('#app').innerHTML = `
 </div>
 </div>
 </div>
-<center>
-<div class="stats shadow">
-  
-  <div class="stat">
-    <div class="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-    </div>
-    <div class="stat-title">Happy Clients</div>
-    <div class="stat-value">100</div>
-  
-    </div>
-  <div class="stat">
-    <div class="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
-    </div>
-    <div class="stat-title">Partners in USA</div>
-    <div class="stat-value">50+</div>
-    <div class="stat-desc">↗︎ 25 (50%)</div>
-  </div>
-  
-  <div class="stat">
-    <div class="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
-    </div>
-    <div class="stat-title">Candy Bars Sold</div>
-    <div class="stat-value">20,000</div>
-    <div class="stat-desc">↗︎ 5000 (25%)</div>
-  </div>
-  
-  <div class="stat">
-  <div class="stat-figure text-secondary">
-  
-  <img src="/images/water.svg" width="40px">
-  </div>
-  <div class="stat-title">Thirsts Quenched</div>
-  <div class="stat-value">32,080</div>
-  <div class="stat-desc">↗︎ 8000 (25%)</div>
-</div>
-</div>
-</center>
-</section>
+
+<div class="divider"></div> 
+
+
+
 <section id="contact">
-
 <div class="contact-box">
-
- 
-  
 <img src="/images/woman.webp" width="45%"/><div class="contact-form-wrapper">
   <form>
     <div class="form-item text-accent text-3xl">
@@ -204,6 +261,9 @@ document.querySelector('#app').innerHTML = `
 </div>
 
 </section>
+
+<div class="divider"></div> 
+
 <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
   <nav class="grid grid-flow-col gap-4">
     <a class="link link-hover">About us</a>
@@ -216,6 +276,13 @@ document.querySelector('#app').innerHTML = `
       <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
       <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
       <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+     <a href="https://www.sincitysweetsllc.com/#" width="24" height="24" class="fill-current">
+     <svg class="fill-current" viewBox="0 0 24 24"width="24" height="24" aria-hidden="true">
+         <path fill-rule="evenodd"
+             d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+             clip-rule="evenodd"></path>
+     </svg>
+ </a>
     </div>
   </nav> 
   <aside>
