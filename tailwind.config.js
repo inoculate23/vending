9d479a9d53104e3ebb7dd6c9cba6cc5ca27899f9
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2024-02-06 17:09:57 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./public/images/**/*.(png,jpg,jpeg}"],
-  plugins: [require('daisyui')],
-
-  // config docs: https://daisyui.com/docs/config/
-  daisyui: {
-    themes: ["light", "dark", "dracula", "night", "dim", "cupcake","pastel", "wireframe", "nord", "retro" , "synthwave", "cyberpunk", "lofi", "business", "aqua", "forest"],
-  },
-  darkTheme: "dark",
-  base: true, // applies background color and foreground color for root element by default
-  styled: true, // include daisyUI colors and design decisions for all components
-  utils: true, // adds responsive and modifier utility classes
-  prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-  logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-  themeRoot: ":root"
-}
-
+    daisyui: {
+      themes: [
+        {
+          mytheme: {
+          
+ "primary": "#2563eb",
+          
+ "secondary": "#d946ef",
+          
+ "accent": "#f59e0b",
+          
+ "neutral": "#374151",
+          
+ "base-100": "#111827",
+          
+ "info": "#38bdf8",
+          
+ "success": "#10b981",
+          
+ "warning": "#fde047",
+          
+ "error": "#dc2626",
+          },
+        },
+      ],
+    },
+    plugins: [
+      require('daisyui'),
+    ],
+    //...
+  }  
